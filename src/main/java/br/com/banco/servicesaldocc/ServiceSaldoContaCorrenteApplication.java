7161425fa2cb7aca598fe7f6e625df8bfe9ae7ab
Thiago Leão <thiago.6286@gmail.com>
2018-10-24 22:53:20 -0300

    Criação da estrutura básica do projeto

---
+++ src/main/java/br/com/banco/servicesaldocc/ServiceSaldoContaCorrenteApplication.java
@@ -0,0 +1,12 @@
+package br.com.banco.servicesaldocc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSaldoContaCorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSaldoContaCorrenteApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/banco/servicesaldocc/ServiceSaldoContaCorrenteApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.banco.servicesaldocc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceSaldoContaCorrenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
